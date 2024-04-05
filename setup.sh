17c2f9c0a312da05a04e1ba76f0a0f4279a2e3e3
Rodrigo Neto <ro.oliveira.neto95@gmail.com>
2024-04-05 19:12:04 -0300

    Add some setup steps and add python to starship prompt

--- setup.sh
+++ setup.sh
@@ -6,7 +6,33 @@ GIT_CREATE_SSH=true
 
 # ---------- Gnome ----------
 sudo dnf install -y gnome-tweaks
-# TODO: set theme, load gsettings, keyboard shortcuts
+
+# Theme
+wget -P /usr/tmp https://github.com/dracula/gtk/archive/master.zip
+mkdir -p $HOME/.themes
+unzip /usr/tmp/master.zip -d $HOME/.themes
+mv $HOME/.themes/gtk-master $HOME/.themes/Dracula
+
+gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
+gsettings set org.gnome.desktop.wm.preferences theme "Dracula"
+
+# Icons
+wget -P /usr/tmp https://github.com/dracula/gtk/files/5214870/Dracula.zip
+mkdir -p $HOME/.icons
+unzip /usr/tmp/Dracula.zip -d $HOME/.icons
+
+gsettings set org.gnome.desktop.interface icon-theme "Dracula"
+
+# TODO: load gsettings, keyboard shortcuts
+
+# ----- Package manager -------
+# Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/rneto/.zshrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo yum groupinstall 'Development Tools'
+
+# dnf from community, Flatpak, ...
 
 # ---------- Terminal ----------
 # Terminator + zsh + oh-my-zsh
@@ -20,7 +46,10 @@ mkdir $HOME/.fonts && \
 	unzip /tmp/FiraMono.zip -d $HOME/.fonts
 curl -sS https://starship.rs/install.sh | sh
 
-# TODO: Atuin
+
+# Atuin
+brew install atuin
+echo 'eval "$(atuin init zsh)"' >> ~/.zshrc
 
 # ---------- Git -----------
 git config --global user.email $GIT_EMAIL
